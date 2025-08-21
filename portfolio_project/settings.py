b4b992054d52fc5bd0ead5e72fb8dc9f9eef678d
surya0523 <surya23051997@gmail.com>
2025-08-22 02:53:42 +0530

    initial commit

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -127,11 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'portfolio/static']
-TEMPLATES[0]['DIRS'] = [BASE_DIR / 'portfolio/templates']
-
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio', 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
